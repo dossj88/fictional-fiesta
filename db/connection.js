1d56b2f1ed578f37b4a06a2a6c1b5871339376b2
jasondoss <jason.j.doss@gmail.com>
2022-12-16 21:56:24 -0800

    added main index.js, addressed bugs causing crash.

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "Doss2022$",
-  database: "test"
+  database: "emp_tracker"
 });
 
 connection.connect(function (err) {
